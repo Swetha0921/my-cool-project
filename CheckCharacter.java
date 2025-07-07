0fba3dbcf513f68da92250a04c11e89913c9d1bd
Swetha v <swethavaiyapuri0912@gmail.com>
2025-07-07 21:17:14 +0530

    CheckCharacter.java

--- CheckCharacter.java
+++ CheckCharacter.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class CheckCharacter
 {
-	public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
+    public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
     char ch=s.next().charAt(0);
     if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z'))
     System.out.println("Alphabet");
