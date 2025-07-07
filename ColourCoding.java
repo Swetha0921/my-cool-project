f86efe963325a2965c25267bb9b180dfb649f6aa
Swetha v <swethavaiyapuri0912@gmail.com>
2025-07-07 22:00:46 +0530

    ColourCoding.java

---
+++ ColourCoding.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ColourCoding
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	char ch=s.next().charAt(0);
+	String colour=(ch=='R')?"Red":(ch=='B')?"Blue":(ch=='O')?"Orange":(ch=='Y')?"Yellow":
+                      (ch=='W')?"White":"Invalid Code";
+    System.out.println(colour);
+	}
+}
