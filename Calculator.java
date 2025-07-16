2ae7ec52c5580ffd980f763e753c5f0ef0f31ddc
Swetha v <swethavaiyapuri0912@gmail.com>
2025-07-16 15:15:37 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Calculator
+{
+     Scanner sc=new Scanner(System.in);
+     int addNumbers()
+    {
+        short n1=sc.nextShort();
+	    short n2=sc.nextShort();
+        return n1+n2;
+    }
+     int subNumbers()
+    {
+         short n1=sc.nextShort();
+	     short n2=sc.nextShort();
+         return n1-n2;
+    }
+     int mulNumbers()
+    {
+         short n1=sc.nextShort();
+	     short n2=sc.nextShort();
+         return n1*n2;
+    }
+     int divNumbers()
+    {
+         short n1=sc.nextShort();
+	     short n2=sc.nextShort();
+         return n1/n2;
+    }
+    
+	public static void main(String[] args) {
+	    Calculator m=new Calculator();
+		System.out.println("The sum of two numbers is "+m.addNumbers());
+		System.out.println("The answer after subtraction of two numbers is "+m.subNumbers());
+		System.out.println("The multiplication of two numbers is "+m.mulNumbers());
+		System.out.println("The division of two numbers is "+m.divNumbers());
+	}
+}
