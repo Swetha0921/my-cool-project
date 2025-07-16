16193ea1f2469c11ea7784f6d822ce372714ed1a
Swetha v <swethavaiyapuri0912@gmail.com>
2025-07-16 15:47:43 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 public class Calculator
 {
-     Scanner sc=new Scanner(System.in);
-     int addNumbers()
+    Scanner sc=new Scanner(System.in);
+    short n1,n2;
+    public void input()
     {
-        short n1=sc.nextShort();
-	    short n2=sc.nextShort();
+        n1=sc.nextShort();
+	    n2=sc.nextShort();
+    }
+    int addNumbers()
+    {
+        input();       
         return n1+n2;
     }
      int subNumbers()
     {
-         short n1=sc.nextShort();
-	     short n2=sc.nextShort();
+           input();       
          return n1-n2;
     }
      int mulNumbers()
     {
-         short n1=sc.nextShort();
-	     short n2=sc.nextShort();
+          input();       
          return n1*n2;
     }
-     float divNumbers()
+    float divNumbers()
     {
-         short n1=sc.nextShort();
-	     short n2=sc.nextShort();
+          input();       
          return n1/n2;
     }
-    
 	public static void main(String[] args) {
 	    Calculator m=new Calculator();
 		System.out.println("The sum of two numbers is "+m.addNumbers());
